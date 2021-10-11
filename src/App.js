1230a20b225cf9ed52ffe686804b90f7aba9cd36
Brian <burbanci@emich.edu>
2021-10-11 14:30:11 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Commerce from "@chec/commerce.js"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY,true);
+    commerce.products.list().then(response=>console.log(response.data));
+    return(
+        <div>
+            <h2>
+                We R Toys
+            </h2>
+           
+        </div>
+    )
 }
 
 export default App;
