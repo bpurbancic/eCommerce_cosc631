c3823adc19270fdaa00d0d6924c89fad8daec214
Brian <burbanci@emich.edu>
2021-10-15 21:02:47 +0000

    Misc changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Commerce from "@chec/commerce.js"
+//import {commerce} from "./lib/commerce"
+// Above line is from 10/6 zoom recording but I can't get it to work, with c or C.
 
 function App() {
     const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY,true);
     commerce.products.list().then(response=>console.log(response.data));
+    
     return(
         <div>
             <h2>
