e603824efc01af178817d33e4136f863fcf87de6
Brian <burbanci@emich.edu>
2021-10-16 00:55:57 +0000

    Continued changes for products assignments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import {commerce} from "./lib/commerce.js"
 // Above line is from 10/6 zoom recording but I can't get it to work, with c or C.
-import {useState} from "react"
+import {useEffect, useState} from "react"
 
 function App() {
-    const [products, setProducts] = useState([]);
-    commerce.products.list().then(result => {
-        console.log(commerce.products.list());
-        console.log("Retrieved the results");
-        setProducts(result.data);
-    });
     
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        commerce.products.list().then(result => {
+            console.log("Retrieved the results");
+            setProducts(result.data);
+        }); 
+    }, []);  // the empty array [] is 2nd argument to useEffect()
+            // It tells React to only re-execute when it detects a change
+
+    //const { productId } = useParams();
+    //const [product, setProduct] = useState([undefined]);
+    //useEffect(() => {
+    //    commerce.products.retrieve(productId).then(result => {
+    //        console.log(result);
+    //        setProduct(result);
+    //    });
+    //}, [productId]);
+    
+  
     return(
         <div>
             <header>
@@ -17,12 +30,17 @@ function App() {
             <h2>Products</h2>
            </header>
            <main>
+               { products.length === 0 && <p>Loading...</p> }
                {
                    products.map((product) => {
                        //return <p key={product.id}>{product.name}</p>
                        // Above should get rid of key error. It didn't work for me.
-                       return <p>{product.name}</p>
-                   })
+                       //return <p>{product.name}</p>  
+                        return <img key={product.id} src={product.image.url} alt={product.name}/>
+                        // Above was taken from a slide image at 52 min of 10/6 zoom video
+                        //return <a href={product.url}><img src={product.image.url} alt={product.name} /></a>
+                        //Above was supposed to make image clickable.
+                    })
                }
            </main>
         </div>  
