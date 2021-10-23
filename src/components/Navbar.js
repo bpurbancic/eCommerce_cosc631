3080af2f6ece1cd236c0b48bcb3ccd1b9ea41cd2
Brian <burbanci@emich.edu>
2021-10-23 13:41:26 +0000

    cart icon and badge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
-import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
+import { AppBar, Badge, Toolbar, Typography, IconButton } from "@material-ui/core";
 import ToysIcon from '@material-ui/icons/Toys';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 function Navbar({cartItems}) {
     return (
         <AppBar position="static" color="primary">
             <Toolbar>
                 <IconButton href="/products"><ToysIcon /></IconButton>
-                <Typography>Products</Typography>
-                {cartItems}
-                    
+                <Typography>We R Toys</Typography>
+                <IconButton>
+                    <Badge badgeContent={cartItems} color="secondary">
+                    <AddShoppingCartIcon />
+                    </Badge>
+                </IconButton>            
             </Toolbar>
         </AppBar>
 
