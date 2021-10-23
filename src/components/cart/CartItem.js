ad6ddc0d61d12f23d0eba6138cc7878c6ec94149
Brian <burbanci@emich.edu>
2021-10-23 16:09:50 +0000

    Added picture, name, quantity, price to cart items

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,11 +1,46 @@
 import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    image: {
+        width: 128,
+        height: 128,
+    },
+    img: {
+        margin: 'auto',
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '80%',
+    },
+    buttons: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+});
 
 function CartItem ({cartItem}) {
+    const classes = useStyles();
     return (
-        <Grid item key={cartItem.id}>
-            {cartItem.name}
-            {"  "}
-            {cartItem.price.formatted_with_symbol}
+        <Grid item container spacing={2}>
+            <Grid item xs={12} sm={4}>
+                <div className={classes.image}>
+                    <img src={cartItem.image.url} className={classes.img}></img>
+                </div>
+            </Grid>
+            <Grid item xs={12} sm={6} container direction="column">
+                <Grid item>
+                    {cartItem.name}
+                </Grid>
+                <Grid item>
+                    quantity: {cartItem.quantity}
+                </Grid>
+                <Grid item>
+                    unit price: {cartItem.price.formatted_with_symbol}
+                </Grid>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+                <div>{cartItem.line_total.formatted_with_symbol}</div>
+            </Grid>
         </Grid>
     );
 }
