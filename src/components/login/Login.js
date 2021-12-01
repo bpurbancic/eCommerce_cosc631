246ec0bfc4d669d433e161200ee4f9b291224bfe
Brian <burbanci@emich.edu>
2021-12-01 23:05:13 +0000

    Added email sent msg to login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -20,23 +20,38 @@ function Login() {
         }
     }
 
+    const [emailSent, setEmailSent] = useState(false);
+
     const handleEmail = (e) => {
         if(email) {
-            commerce.customer.login(email, window.location.href+"/Login").then((response) => console.log(response))
+            commerce.customer.login(email, window.location.href+"/Login").then((response) => console.log(response));
+            setEmailSent(true);            
         }
+    }
+    if (emailSent) {
+        return (
+            <Grid container direction="column">
+            <Grid item><h3>Login</h3></Grid>
+
+            <Grid item><h4>An email is sent to your email address.</h4></Grid>
+
+        </Grid>
+            
+        )
     }
    
     return (
         <Grid container direction="column">
             <Grid item><h3>Login</h3></Grid>
-            
+
             <Grid item>
-                <TextField label = "Email Address" onChange = {onEmailChange}
+                <TextField label = "Enter email" onChange = {onEmailChange}
                     error = {emailError}
                     helperText={emailHelper}
                     onBlur = {onEmailUnfocused}
                 />
             </Grid>
+        
             <Grid item><Button size = "xsmall" onClick={handleEmail}>Request Login Credentials</Button></Grid>
             
         </Grid>
@@ -45,3 +60,22 @@ function Login() {
 }
 
 export default Login;
+
+// const [isSent, setIsSent] = useState(false);
+// if(isSent) {
+//     return <h3> An email is sent to {loginEmail}</h3>
+// }
+
+// setIsSent(true) in your handleemail  after console.log
+
+// state = { showing: true };
+
+//     render() {
+//         const { showing } = this.state;
+//         return (
+//             <div>
+//                 <button onClick={() => this.setState({ showing: !showing })}>toggle</button>
+//                 <div style={{ display: (showing ? 'block' : 'none') }}>This is visible</div>
+//             </div>  
+//         )
+//     }
