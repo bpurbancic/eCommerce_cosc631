e3648dfc0aed548217b8375a3a45b9f27d30d3e6
Brian <burbanci@emich.edu>
2021-12-16 19:54:14 +0000

    Added update to profile page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,32 +1,124 @@
 import { commerce } from "../../lib/commerce";
 import { useEffect, useState } from "react";
+import { Button, Grid, MenuItem, Select, TextField } from "@material-ui/core";
+import ReactPhoneInput from 'react-phone-input-material-ui';
 
 function Profile() {
     // Note: Customer should always be logged when they come to this page.
     // But to be safe we check login status anyway.
 
-    const [fullName, setFullName] = useState("");
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
-
+    const [custID, setCustID] = useState("");
+    
     useEffect (() => {
         commerce.customer.about().then((customer) => {
             console.log(customer);
-            setFullName(customer["firstname"] + " " + customer["lastname"]);
+            setName(customer["firstname"] + " " + customer["lastname"]);
             setEmail(customer["email"]);
             setPhone(customer["phone"]);
+            setCustID(customer["id"]);
             
         });
-      });
+    },[]);
+
+    const [nameError, setNameError] = useState(false);
+    const [nameHelper, setNameHelper] = useState("");
+
+    const onNameChange = (e) => {setName(e.target.value)}
+
+    const onNameUnfocused = (e) => { 
+        if (!name) {
+            setNameError(true);
+            setNameHelper("Name cannot be empty.")
+        } else { 
+            setNameError(false);
+            setNameHelper("");
+        }
+    }
+
+    const [emailError, setEmailError] = useState(false);
+    const [emailHelper, setEmailHelper] = useState("");
+
+    const onEmailChange = (e) => {setEmail(e.target.value)}
+
+    const onEmailUnfocused = (e) => { 
+        if (!email) {
+            setEmailError(true);
+            setEmailHelper("Email cannot be empty.")
+        } else { 
+            setEmailError(false);
+            setEmailHelper("");
+        }
+    }
+
+    const [phoneError, setPhoneError] = useState(false);
+    const [phoneHelper, setPhoneHelper] = useState("");
+
+    const onPhoneChange = (e) => {setPhone(e.target.value)}
+
+    const onPhoneUnfocused = () => { 
+        if (!phone) {
+            setPhoneError(true);
+            setPhoneHelper("Phone cannot be empty.")
+        } else { 
+            setPhoneError(false);
+            setPhoneHelper("");
+        }
+    }
+
+    function handleUpdate(){
+        let nameSplit = name.split(' ');
+        let first = nameSplit[0];
+        let last = nameSplit[nameSplit.length-1];
+
+        commerce.customer.update({
+            email: email,
+            firstname: first,
+            lastname: last,
+            phone: phone
+            
+          }, custID).then((customer) => console.log(customer));
+    }
 
     return (
-        <div>
-            <h2>Profile Information</h2>
-            <p>{fullName}</p>
-            <p>{email}</p>
-            <p>{phone}</p>
-        </div>
+        <Grid container direction="column">
+            <Grid item><h3>Profile Information</h3></Grid>
+            
+            <Grid item>
+                <TextField label = {name} onChange = {onNameChange}
+                    error = {nameError}
+                    helperText={nameHelper}
+                    onBlur = {onNameUnfocused}
+                />
+               
+            </Grid>
+            
+            <Grid item>
+                <TextField label = {email} onChange = {onEmailChange}
+                    error = {emailError}
+                    helperText={emailHelper}
+                    onBlur = {onEmailUnfocused}
+                />
+            </Grid>
+            
+            <Grid item>
+                <TextField label={phone} onChange={onPhoneChange}
+                    error = {phoneError}
+                    helperText={phoneHelper}
+                    onBlur = {onPhoneUnfocused}     
+                            
+                       
+                /> 
+            </Grid>
+            <Grid item>
+            <Button onClick={handleUpdate} color='primary' size='small' >Update Profile</Button>
+            
+            </Grid>
+        </Grid>
     )
+   
 
 }
 
